be7d68cb3ac2c064720d28a84e6006e44d91fa64
gil <longlongroad@naver.com>
2022-11-05 23:13:24 +0900

    [MODIFY]  - COIN CHECKER

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import { useEffect, useState } from "react";
+import styles from "./AppStyle.module.css";
 
 function App() {
-  const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
       .then((json) => {
         setCoins(json);
-        console.log(coins);
       });
   }, []);
+  function CoinList({ coin }) {
+    return (
+      <ul className={styles.liContainer}>
+        {coin.map((arr) => (
+          <li>
+            <strong>
+              {arr.symbol}
+              <span>({arr.name})</span>
+            </strong>
+            <span className="price">
+              <em>$</em> {arr.quotes.USD.price}
+            </span>
+          </li>
+        ))}
+      </ul>
+    );
+  }
   return (
-    <div>
-      <h2>
+    <div className={styles.bodyWrap}>
+      <h2 className={styles.title}>
         <img src="/img/coin.png" alt="" />
       </h2>
-      <div></div>
+
+      <div className={styles.liWrap}>
+        <h3>Coins List ({coins.length})</h3>
+        <CoinList coin={coins} />
+      </div>
     </div>
   );
 }
